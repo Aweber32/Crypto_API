12f76d94139dfd02f067aa724f5b6b25b4067633
Alex Weber <alexweber12899@gmail.com>
2025-06-15 21:14:46 -0400

    coin data update

---
+++ CryptoApi/Migrations/20250616011332_updatingcoindata.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatingcoindata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VolumeChange24h",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Volume24h",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange90d",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange7d",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange60d",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange30d",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange24h",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PercentChange1h",
+                table: "CoinDatas",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VolumeChange24h",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Volume24h",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange90d",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange7d",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange60d",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange30d",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange24h",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PercentChange1h",
+                table: "CoinDatas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
--- CryptoApi/Models/Coin_Data.cs
+++ CryptoApi/Models/Coin_Data.cs
@@ -9,14 +9,14 @@ namespace CryptoApi.Models
         public string Symbol { get; set; }
         public DateTime Date { get; set; } 
         public string? TokenName { get; set; }
-        public string? Price { get; set; }
-        public string? Volume24h { get; set; }
-        public string? VolumeChange24h { get; set; }
-        public string? PercentChange1h { get; set; }
-        public string? PercentChange24h { get; set; }
-        public string? PercentChange7d { get; set; }
-        public string? PercentChange30d { get; set; }
-        public string? PercentChange60d { get; set; }
-        public string? PercentChange90d { get; set; }
+        public double? Price { get; set; }
+        public double? Volume24h { get; set; }
+        public double? VolumeChange24h { get; set; }
+        public double? PercentChange1h { get; set; }
+        public double? PercentChange24h { get; set; }
+        public double? PercentChange7d { get; set; }
+        public double? PercentChange30d { get; set; }
+        public double? PercentChange60d { get; set; }
+        public double? PercentChange90d { get; set; }
     }
 }
